18b36fcb86813ca3baf29a0b683915202f287dbf
Vijay Karthik <vijaykarthiku7@gmail.com>
2026-09-11 19:42:28 +0530

    P

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
 
   useEffect(() => {
     document.title = pageTitle
-    const faviconHref = `${import.meta.env.BASE_URL}logo-assets/favicon-32.png?v=fav-2`
+    const faviconHref = `${import.meta.env.BASE_URL}logo-assets/turfon24-favicon.svg?v=fav-3`
     let favicon = document.querySelector('link[data-turfon24-favicon]')
     if (!favicon) {
       favicon = document.createElement('link')
